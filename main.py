e44ce4b610b7d200d81eaaa080400201cf95d007
Rifat Fazlutdinov <46864568+Rifat-Fazlutdinov@users.noreply.github.com>
2020-06-25 18:06:59 +0500

    adds method perfom_move and algorithm of finding available moves was tested

--- main.py
+++ main.py
@@ -8,4 +8,6 @@ r = Rook('2d', 'rook', 'R')
 
 r.team = 'black'
 
+print(r.perfome_move(chess_field, '6d'))
+print(r.coord)
 print(r._available_moves(chess_field))
--- models.py
+++ models.py
@@ -25,6 +25,11 @@ class Rook(BaseFigure):
 
     def perfome_move(self, field, coords_to):
         available_coords_to_move = self._available_moves(field)
+        if coords_to not in available_coords_to_move:
+            return 'Not available move'
+        else:
+            self.coord = coords_to
+            return 'Moved!'
     
     def _available_moves(self, field):
 
