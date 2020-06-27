5846871bb55f99506b1d5dee2a2f1a5676eebda6
Rifat Fazlutdinov <46864568+Rifat-Fazlutdinov@users.noreply.github.com>
2020-06-27 10:34:59 +0500

    adds adding move on the W side

--- models/king.py
+++ models/king.py
@@ -72,5 +72,17 @@ class King(BaseFigure):
             moves.append(str(int(self.coord[0]) + 1) + chr(ord(self.coord[1]) - 1))
         except KeyError as e:
             pass
+
+        try:
+            if field.field[str(int(self.coord[0])) + chr(ord(self.coord[1]) - 1)].team == self.team and int(self.coord[0] + 1) >= 9:
+                pass
+            else:
+                moves.append(str(int(self.coord[0])) + chr(ord(self.coord[1]) - 1))
+        except AttributeError as e:
+            moves.append(str(int(self.coord[0])) + chr(ord(self.coord[1]) - 1))
+        except KeyError as e:
+            pass
+
+        
         return moves
 
