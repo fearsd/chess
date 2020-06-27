496948890e1839cdb49de2de756f409f0ed19ea3
Rifat Fazlutdinov <46864568+Rifat-Fazlutdinov@users.noreply.github.com>
2020-06-27 15:36:19 +0500

    adds adding to moves coords on WN side

--- models/king.py
+++ models/king.py
@@ -83,6 +83,15 @@ class King(BaseFigure):
         except KeyError as e:
             pass
 
-        
+        try:
+            if field.field[str(int(self.coord[0]) - 1) + chr(ord(self.coord[1]) - 1)].team == self.team:
+                pass
+            else:
+                moves.append(str(int(self.coord[0]) - 1) + chr(ord(self.coord[1]) - 1))
+        except AttributeError as e:
+            moves.append(str(int(self.coord[0]) - 1) + chr(ord(self.coord[1]) - 1))
+        except KeyError as e:
+            pass
+
         return moves
 
