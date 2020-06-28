3029c4cd5b0d50bda827b7d0f352490c3c96ad9f
Rifat Fazlutdinov <46864568+Rifat-Fazlutdinov@users.noreply.github.com>
2020-06-28 11:34:17 +0500

    refactoring code of finding moves on other sides in Rook

--- models/rook.py
+++ models/rook.py
@@ -9,6 +9,7 @@ class Rook(BaseFigure):
         rev = list(field.field.keys())
         rev.reverse()
 
+
         top = []
         try:
             if field.field[str(int(self.coord[0]) - 1) + self.coord[1]].team == self.team:
@@ -23,7 +24,68 @@ class Rook(BaseFigure):
                 if (int(c[0]) < int(top[-1][0]) and c[1] == top[-1][1]):
                     top.append(c)
 
+        i = 0
+        while i <= len(top) - 1:
+            if isinstance(field.field[top[i]], BaseFigure):
+                try:
+                    del top[i + 1]
+                except IndexError:
+                    break
+            else:
+                i += 1
+
         moves += top.copy()
+
+
+        down = []
+        try:
+            if field.field[str(int(self.coord[0]) + 1) + chr(ord(self.coord[1]))].team == self.team:
+                down.append(str(int(self.coord[0]) + 1) + chr(ord(self.coord[1])))
+        except AttributeError as e:
+            down.append(str(int(self.coord[0]) + 1) + chr(ord(self.coord[1])))
+        except KeyError as e:
+            pass
+
+        if not len(down) == 0:
+            for c in keys:
+                if (int(c[0]) > int(down[-1][0]) and c[1] == down[-1][1]):
+                    down.append(c)
+        
+        moves += down.copy()
+
+
+        left = []
+        try:
+            if field.field[str(int(self.coord[0])) + chr(ord(self.coord[1]) - 1)].team == self.team:
+                left.append(str(int(self.coord[0])) + chr(ord(self.coord[1]) - 1))
+        except AttributeError as e:
+            left.append(str(int(self.coord[0])) + chr(ord(self.coord[1]) - 1))
+        except KeyError as e:
+            pass
+
+        if not len(left) == 0:
+            for c in rev:
+                if (int(c[0]) == int(left[-1][0]) and ord(c[1]) < ord(left[-1][1])):
+                    left.append(c)
+
+        moves += left.copy()
+
+
+        right = []
+        try:
+            if field.field[str(int(self.coord[0])) + chr(ord(self.coord[1]) + 1)].team == self.team:
+                right.append(str(int(self.coord[0])) + chr(ord(self.coord[1]) + 1))
+        except AttributeError as e:
+            right.append(str(int(self.coord[0])) + chr(ord(self.coord[1]) + 1))
+        except KeyError as e:
+            pass
+
+        if not len(right) == 0:
+            for c in keys:
+                if (int(c[0]) == int(right[-1][0]) and ord(c[1]) > ord(left[-1][1])):
+                    right.append(c)
+
+        moves += right.copy()
 
         # # finding all coords whose rook can move to if these coord would be empty
         # moves = []
