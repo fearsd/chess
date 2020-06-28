9aa190e1f9c853f0816098916d8c9be735cccf5b
Rifat Fazlutdinov <46864568+Rifat-Fazlutdinov@users.noreply.github.com>
2020-06-28 21:25:10 +0500

    adds modes of game

--- main.py
+++ main.py
@@ -3,8 +3,20 @@ from models.player import Player, Computer
 
 def game_init():
     chess = GameField()
-    white_player = Player('user1', 'white', chess)
-    black_player = Computer('AI', 'black', chess)
+    modes = (1, 2, 3)
+    mode = 9
+    while mode not in modes:
+        mode = int(input('Choose mode of the game: \n1: human vs human\n2: computer vs human\n3: computer vs computer'))
+
+    if mode == 1:
+        white_player = Player('user1', 'white', chess)
+        black_player = Player('user2', 'black', chess)
+    elif mode == 2:
+        white_player = Player('user', 'white', chess)
+        black_player = Computer('snapdragon 865', 'black', chess)
+    else:
+        white_player = Computer('core i5', 'white', chess)
+        black_player = Computer('snapdragon 865', 'black', chess)
 
     return chess, white_player, black_player
 
