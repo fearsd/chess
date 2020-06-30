cf873396a256d32d5c5c5c63e3daf1889b74d72d
Rifat Fazlutdinov <46864568+Rifat-Fazlutdinov@users.noreply.github.com>
2020-06-30 17:33:59 +0500

    refactoring tests

--- tests.py
+++ tests.py
@@ -5,17 +5,28 @@ from models.gamefield import GameField
 
 class TestBishop(unittest.TestCase):
 
-    def test_moves_on_1a(self):
+    def test_moves_on_1a_when_figure_is_white(self):
+        # creating Bishop instance
+        b = Bishop('1a', 'bishop', 'b')
+        # creating GameField instance
+        g = GameField()
+        self.assertEqual(b._available_moves(g), [])
+
+        # moving teammate pawn
+        g.field['2b'].perform_move(g, '3b')
+        self.assertEqual(b._available_moves(g), ['2b', '3c', '4d', '5e', '6f', '7g'])
+
+        # moving enemy pawn
+        g.field['7g'].perform_move(g, '6g')
+        self.assertEqual(b._available_moves(g), ['2b', '3c', '4d', '5e', '6f', '7g', '8h'])
+
+    def test_moves_on_1a_when_figure_is_black(self):
+        # creating Bishop instance
         b = Bishop('1a', 'bishop', 'b')
         b.team = 'black'
-        self.assertEqual(b._available_moves(GameField()), ['2b'])
+        # creating GameField instance
         g = GameField()
-        g.field['2b'].perform_move(g, '3b')
-        self.assertEqual(b._available_moves(g), ['2b', '3c', '4d', '5e', '6f'])
-        g.field['7g'].perform_move(g, '6g')
-        g.field['7h'].perform_move(g, '6h')
-        g.field['8h'].perform_move(g, '7h')
-        self.assertEqual(b._available_moves(g), ['2b', '3c', '4d', '5e', '6f', '7g', '8h'])
+        self.assertEqual(b._available_moves(g), ['2b'])
 
 
 if __name__ == "__main__":
