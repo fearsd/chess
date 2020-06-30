c4448e6787a447c152fb183b2bfdc3db5f402f68
Rifat Fazlutdinov <46864568+Rifat-Fazlutdinov@users.noreply.github.com>
2020-06-30 13:39:46 +0500

    cosmetic changes

--- main.py
+++ main.py
@@ -6,17 +6,23 @@ def game_init():
     modes = (1, 2, 3)
     mode = 9
     while mode not in modes:
-        mode = int(input('Choose mode of the game: \n1: human vs human\n2: computer vs human\n3: computer vs computer'))
+        mode = int(input('Choose mode of the game: \n1: human vs human\n2: computer vs human\n3: computer vs computer \n: '))
 
     if mode == 1:
-        white_player = Player('user1', 'white', chess)
-        black_player = Player('user2', 'black', chess)
+        name1 = input('Input name of first (white) player: ')
+        name2 = input('Input name of second (black) player: ')
+        white_player = Player(name1, 'white', chess)
+        black_player = Player(name2, 'black', chess)
     elif mode == 2:
-        white_player = Player('user', 'white', chess)
-        black_player = Computer('snapdragon 865', 'black', chess)
+        name1 = input('Input name of first (white) player: ')
+        name2 = input('Input name of second (black) player (computer): ')
+        white_player = Player(name1, 'white', chess)
+        black_player = Computer(name2, 'black', chess)
     else:
-        white_player = Computer('core i5', 'white', chess)
-        black_player = Computer('snapdragon 865', 'black', chess)
+        name1 = input('Input name of first (white) player (computer): ')
+        name2 = input('Input name of second (black) player (computer): ')
+        white_player = Computer(name1, 'white', chess)
+        black_player = Computer(name2, 'black', chess)
 
     return chess, white_player, black_player
 
