20bdb4b8617b824f8d158e57a7045fa13b6a6b9d
Rifat Fazlutdinov <46864568+Rifat-Fazlutdinov@users.noreply.github.com>
2020-06-30 19:26:19 +0500

    adds tests when bishop on 1b

--- tests.py
+++ tests.py
@@ -61,6 +61,7 @@ class TestBishop(unittest.TestCase):
         g = GameField()
         self.assertEqual(b._available_moves(g), ['2b'])
 
+
     def test_moves_on_1a_when_figure_is_black_and_white_pawn_on_2b_is_moved_to_3b(self):
         b = Bishop('1a', 'bishop', 'b')
         b.team = 'black'
@@ -68,5 +69,44 @@ class TestBishop(unittest.TestCase):
         g = GameField()
         g.field['2b'].perform_move(g, '3b')
         self.assertEqual(b._available_moves(g), ['2b', '3c', '4d', '5e', '6f'])
+
+    
+    def test_moves_on_1b_when_figure_is_white_and_no_one_is_moved(self):
+        b = Bishop('1b', 'bishop', 'b')
+        # creating GameField instance
+        g = GameField()
+        self.assertEqual(b._available_moves(g), [])
+
+
+    def test_moves_on_1b_when_figure_is_white_and_white_pawn_on_2c_is_moved_to_3c(self):
+        b = Bishop('1b', 'bishop', 'b')
+        g = GameField()
+        g.field['2c'].perform_move(g, '3c')
+        self.assertEqual(b._available_moves(g), ['2c', '3d', '4e', '5f', '6g', '7h'])
+    
+
+    def test_moves_on_1b_when_figure_is_white_and_white_pawn_on_2c_is_moved_to_3c_and_white_pawn_on_2a_is_moved_to_3a(self):
+        b = Bishop('1b', 'bishop', 'b')
+        g = GameField()
+        g.field['2c'].perform_move(g, '3c')
+        g.field['2a'].perform_move(g, '3a')
+        self.assertEqual(b._available_moves(g), ['2c', '3d', '4e', '5f', '6g', '7h', '2a'])
+
+
+    def test_moves_on_1b_when_figure_is_black_and_no_one_is_moved(self):
+        b = Bishop('1b', 'bishop', 'b')
+        b.team = 'black'
+        g = GameField()
+        self.assertEqual(b._available_moves(g), ['2c', '2a'])
+
+    
+    def test_moves_on_1b_when_figure_is_black_and_white_pawn_on_2c_is_moved_to_3c(self):
+        b = Bishop('1b', 'bishop', 'b')
+        b.team = 'black'
+        g = GameField()
+        g.field['2c'].perform_move(g, '3c')
+        self.assertEqual(b._available_moves(g), ['2c', '3d', '4e', '5f', '6g', '2a'])
+
+
 if __name__ == "__main__":
     unittest.main()
