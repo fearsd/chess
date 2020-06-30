bae188b8cd1a265b2801f117a90a7810a6e9931b
Rifat Fazlutdinov <46864568+Rifat-Fazlutdinov@users.noreply.github.com>
2020-06-30 18:54:44 +0500

    adds tests for black bishop on 1a

--- tests.py
+++ tests.py
@@ -53,18 +53,20 @@ class TestBishop(unittest.TestCase):
         self.assertEqual(b._available_moves(g), ['2b', '3c', '4d', '5e', '6f', '7g', '8h'])
 
 
-    # def test_moves_on_1a_when_figure_is_black(self):
-    #     # creating Bishop instance
-    #     b = Bishop('1a', 'bishop', 'b')
-    #     # creating GameField instance
-    #     g = GameField()
-    #     # creating Bishop instance
-    #     b = Bishop('1a', 'bishop', 'b')
-    #     b.team = 'black'
-    #     # creating GameField instance
-    #     g = GameField()
-    #     self.assertEqual(b._available_moves(g), ['2b'])
+    def test_moves_on_1a_when_figure_is_black_and_no_one_is_moved(self):
+        # creating Bishop instance
+        b = Bishop('1a', 'bishop', 'b')
+        b.team = 'black'
+        # creating GameField instance
+        g = GameField()
+        self.assertEqual(b._available_moves(g), ['2b'])
 
-
+    def test_moves_on_1a_when_figure_is_black_and_white_pawn_on_2b_is_moved_to_3b(self):
+        b = Bishop('1a', 'bishop', 'b')
+        b.team = 'black'
+        # creating GameField instance
+        g = GameField()
+        g.field['2b'].perform_move(g, '3b')
+        self.assertEqual(b._available_moves(g), ['2b', '3c', '4d', '5e', '6f'])
 if __name__ == "__main__":
     unittest.main()
